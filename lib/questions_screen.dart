fe90a016115efc9d973e21d715006a0953278561
Dmitry Inke <angranity1998@gmail.com>
2023-08-27 23:29:48 +0300

    implemented start quiz button, created new window <questions screen>

---
+++ lib/questions_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
+
+  @override
+  State<QuestionsScreen> createState() => _QuestionsScreen();
+}
+
+class _QuestionsScreen extends State<QuestionsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffledAnswers().map(
+                  (String answer) => AnswerButton(answer, () {}),
+                ),
+          ],
+        ),
+      ),
+    );
+  }
+}
